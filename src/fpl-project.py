d45d4b6f23972b72979ebd4b07292e03fcc916aa
Prannvat Singh <prannvat2005@gmail.com>
2022-10-18 14:00:38 +0100

    Started working on neural network

--- src/fpl-project.py
+++ src/fpl-project.py
@@ -1,14 +1,8 @@
-from difflib import diff_bytes
-from enum import Enum, auto
-from venv import create
-from xml.dom.minidom import Element
 import requests , json
 import pandas as pd
 import numpy as np 
 from pprint import pprint
-import matplotlib.pyplot as plt
-from pandas.plotting import scatter_matrix
-import sqlite3
+
 from sqlalchemy import create_engine
 from PIL import Image
 import kivy
@@ -17,8 +11,10 @@ from kivy.app import App #the base class must inherit ftom the App class
 from kivy.uix.label import Label   
 from kivy.uix.gridlayout import GridLayout 
 from kivy.uix.textinput import TextInput
-from scipy.special import expit as activation_function
-from scipy.stats import truncnorm
+
+from tensorflow.python.keras.models import Sequential
+
+from tensorflow.python.keras.layers import Dense
 
 
 
@@ -47,12 +43,13 @@ def every_gameweek_for_player_df(user_choice_player):
     # ^^sending GET request to https://fantasy.premierleague.com/api/element-summary/{PID}/
     fixtures_df = pd.json_normalize(element_summary_URL['fixtures'])
     fixtures_df[['difficulty']]
-    fixtures_df['difficulty']
     specific_player_df = pd.json_normalize(element_summary_URL['history'])
      
     specific_player_df = pd.merge(specific_player_df,fixtures_df[['difficulty','is_home']], left_on= 'round',right_on = 'difficulty')
-    specific_player_df = specific_player_df[['difficulty','is_home','total_points','minutes','goals_scored','assists']]
+    # specific_player_df = specific_player_df[['difficulty','is_home','total_points','minutes','goals_scored','assists']]
+    specific_player_df = specific_player_df[['difficulty']]
     return specific_player_df
+    
 
 def current_season_gameweek_info(user_choice_player):
    
@@ -72,7 +69,9 @@ def past_seasons_info(user_choice_player_history):
     specific_player_df_history = pd.json_normalize(request_URL['history_past'])
     return specific_player_df_history
 
-def weekly_stats_df(): # makes a table with all the stats
+
+
+def weekly_stats_df(): # makes a table with all the stats of all players in each gameweek seperately
     request_URL = requests.get(main_url+'bootstrap-static/').json()
     # request_URL = requests.get(main_url+'event/'+str(gameweek)+'/live/').json()
     # I can now get player data usimg the 'elements' field of the API:
@@ -104,16 +103,15 @@ def weekly_stats_df(): # makes a table with all the stats
     writer = pd.ExcelWriter('player_points_weekly.xlsx')
     player_points.to_excel(writer)
     writer.save()
+    # scatter_matrix(player_points[['total_points','opponent_team','transfers_in']])
+    # plt.xticks(rotation=90)
+    # plt.show()
     return player_points
 
-def total_stats_sum_df():
+def total_stats_sum_df(): # this gives the sum of everything and orders the stats in most points scored.
     total_stats_season_df = weekly_stats_df()
     total_stats_season_df = total_stats_season_df.groupby(['element','web_name', 'singular_name_short']).agg({'total_points':'sum', 'goals_scored':'sum', 'assists':'sum',
     'goals_conceded':'sum'}).reset_index().sort_values('total_points', ascending=False) 
-
-    # print(pd.plotting.scatter_matrix(total_stats_season_df[['total_points','goals_scored','assists','goals_conceded']]))
-    # plt.xticks()
-    # plt.show()
     writer = pd.ExcelWriter('player_points.xlsx')
     total_stats_season_df.to_excel(writer)
     writer.save()
@@ -130,64 +128,50 @@ def getting_player_images():
         
 def search(): 
 
+
         
     player_points = weekly_stats_df()
     engine = create_engine('sqlite://', echo=False)
     player_points.to_sql('players',engine, if_exists='replace', index=False)
     user_search_player = input(str("Enter player name: "))
+    #using sql, i can ask for a user input of what player they want, and that players data is accessed.
     results = engine.execute("Select * from players where web_name= (?)",user_search_player)
     one_player_df = pd.DataFrame(results, columns=player_points.columns)
     player_stats_df = user_search_player+'.xlsx'
     one_player_df.to_excel(player_stats_df, index=False)
     return one_player_df
+   
+def getting_dataset_to_train_nnetwork():
+    #i need a list of all my data in a manner which i can then use a model on.
+    training_df = weekly_stats_df()
+    training_df = training_df[['id_player','opponent_team', 'was_home','round','transfers_in', 'selected','total_points']]
+    training_df['was_home'] = training_df['was_home'].astype(int)
+    # making a dataframe first using previous function and then changing into list.
+    training_list = training_df.values.tolist()
+    
+    
+    return training_list
 
+def keras_model():
+    dataset = getting_dataset_to_train_nnetwork()
+    x = dataset[:,0:5]
+    y = dataset[:,6]
+    model = Sequential()
+    #to work out how many layers will be good the best thing to do is experimentation:
+    
+    #using three layers for now,
+    ''' first layer will have 12 nodes
+        second layer will have 8 nodes
+        third layer will have  1 node   '''
 
-def truncated_normal(mean=0, sd=1, low=0, upp=10):
-    return truncnorm(
-        (low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
-class NeuralNetwork:
-    def __init__(self, 
-                    no_of_in_nodes, 
-                    no_of_out_nodes, 
-                    no_of_hidden_nodes,
-                    learning_rate):
-            self.no_of_in_nodes = no_of_in_nodes
-            self.no_of_out_nodes = no_of_out_nodes
-            self.no_of_hidden_nodes = no_of_hidden_nodes
-            self.learning_rate = learning_rate 
-            self.create_weight_matrices()
-            
-    def create_weight_matrices(self):
-            """ A method to initialize the weight matrices of the neural network"""
-            rad = 1 / np.sqrt(self.no_of_in_nodes)
-            X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
-            self.weights_in_hidden = X.rvs((self.no_of_hidden_nodes, 
-                                        self.no_of_in_nodes))
-            rad = 1 / np.sqrt(self.no_of_hidden_nodes)
-            X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
-            self.weights_hidden_out = X.rvs((self.no_of_out_nodes, 
-                                            self.no_of_hidden_nodes))
-    def train(self, input_vector, target_vector):
-            pass # More work is needed to train the network
-
-    def run(self, input_vector):
-            """
-            running the network with an input vector 'input_vector'. 
-            'input_vector' can be tuple, list or ndarray
-            """
-            # Turn the input vector into a column vector:
-            input_vector = np.array(input_vector, ndmin=2).T
-            # activation_function() implements the expit function,
-            # which is an implementation of the sigmoid function:
-            input_hidden = activation_function(self.weights_in_hidden @   input_vector)
-            output_vector = activation_function(self.weights_hidden_out @ input_hidden)
-            return output_vector 
-    # Initialize an instance of the class:  
-simple_network = NeuralNetwork(no_of_in_nodes=2,         
-                            no_of_out_nodes=2, 
-                            no_of_hidden_nodes=4,
-                            learning_rate=0.6)
-
+    model.add(Dense(12, input_shape=(6,)), activation='relu')
+    #using the ReLU activation function as better performance is acheieved using it compared to Tanh function or just the sigmoid function.
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    #compiling the training model. 
+    # I am using the 'adam' optimizer (version of gradient descent)
+    # I am using cross entropy as the loss argument.
+    model.compile(loss = 'binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 if __name__ == '__main__':
     # game_week_info()
@@ -197,13 +181,5 @@ if __name__ == '__main__':
     
     
     request_URL = requests.get(main_url+'element-summary/9/').json() 
-    # past_seasons_info(4)
-    # (current_season_gameweek_info(4)[['round','difficulty','total_points','minutes','goals_scored','assists']])
-    # print(current_season_gameweek_info(80))
-    # using 4 as a random place holder, same with 2 below
-    # print(past_seasons_info(427)[['season_name','total_points','minutes','goals_scored','assists']])
-    
-    # print(weekly_stats_df())
-    print(simple_network.run([(3, 4)]))
-    
-    # (every_gameweek_for_player_df(318))
+   
+    print(getting_dataset_to_train_nnetwork())
